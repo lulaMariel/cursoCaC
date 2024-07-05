199ebdd50376ec1c193103672a0976f07698dddc
lulaMariel <luciana.lavalle@gmail.com>
2024-07-05 19:29:08 -0300

    clase 17-04 hasta minut0 14:12

---
+++ js-02/js/script.js
@@ -0,0 +1,28 @@
+let num1 = 5;
+
+// num1 = num1 + 1;
+
+// console.log(num1);
+
+// console.log(2 == 2);
+// console.log(2 == "2");
+// console.log(2 === "2");
+
+// console.log(2 != 2);
+// console.log(2 != 3);
+// console.log(2 !== "2");
+
+// console.log(2 > 5);
+// console.log(12 > 5);
+
+// console.log(22 < 5);
+
+// console.log(2 >= 2);
+
+// console.log(5 <= 5);
+
+let num = 3;
+let objeto = new Number(3);
+
+console.log(num == objeto);
+console.log(num === objeto);
