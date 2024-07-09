ff7f55bb7d101361f8db6c73b2a96199bd679dd7
lulaMariel <luciana.lavalle@gmail.com>
2024-07-09 13:32:09 -0300

    Clase 17-04 minuto 28:06

--- js-02/js/script.js
+++ js-02/js/script.js
@@ -1,8 +1,12 @@
 let num1 = 5;
 
-// num1 = num1 + 1;
+num1 = num1 + 1;
+num1 += 1;
+num1++;
 
-// console.log(num1);
+console.log(num1); // 8
+console.log(num1++); // 9
+console.log(++num1); // 10
 
 // console.log(2 == 2);
 // console.log(2 == "2");
@@ -21,8 +25,21 @@ let num1 = 5;
 
 // console.log(5 <= 5);
 
-let num = 3;
-let objeto = new Number(3);
+// let num = 3;
+// let objeto = new Number(3);
 
-console.log(num == objeto);
-console.log(num === objeto);
+// console.log(num == objeto);
+// console.log(num === objeto);
+
+// Lógicos
+
+// let num = 4;
+
+// console.log(num >= 2 && num <= 5);
+
+// let cantidad = 3;
+// let tarjeta = "Naranja";
+
+// console.log(cantidad >= 3 || tarjeta == "Visa");
+
+// console.log(!(2 > 1));
