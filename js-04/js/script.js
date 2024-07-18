7392a8fd34d82fc266dae0eacb799f71c0ff3808
lulaMariel <luciana.lavalle@gmail.com>
2024-07-18 00:35:48 -0300

    clase 24/04 hasta minuto 14:41

---
+++ js-04/js/script.js
@@ -0,0 +1,20 @@
+// let name = "Usuario 1";
+// let password = "holamundo";
+// let email = "x@x.com";
+// let admin = true;
+
+// const user = new Object();
+
+// user.name = "Usuario 1";
+// user.password = "holamundo";
+// user.email = "x@x.com"
+// user.admin = true;
+
+const user = {
+    name: "Usuario 1",
+    password: "holamundo",
+    email: "x@x.com",
+    admin: true,
+}
+
+console.log(user, typeof user);
